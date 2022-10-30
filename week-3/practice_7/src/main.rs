b01787444953ea857fe3aeef70bbf95610bd6f75
N-osujicsc101 <115807216+N-osujicsc101@users.noreply.github.com>
2022-10-30 15:38:57 +0100

    (my third commit)

--- week-3/practice_7/src/main.rs
+++ week-3/practice_7/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let float_with_separator = 11_000.555_001;
-    println!("float value {}",float_with_separator);
+    println!("My float value {}",float_with_separator);
 
 
     let int_with_separator = 50_000;
