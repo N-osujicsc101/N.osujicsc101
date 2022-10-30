ab2f5b6fb065934acc083a13190ef0404039e40c
N-osujicsc101 <115807216+N-osujicsc101@users.noreply.github.com>
2022-10-30 15:35:19 +0100

    (my second commit)

--- week-2/practice_2.rs
+++ week-2/practice_2.rs
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 fn main() {
-	println!("i am a material girl");
-=======
-fn main() {
-	println!("i am a material girl");
->>>>>>> eb1ca07c7ba2539e965be1a48075467c22442c4f
+	println!("i am a fine material girl");
 }
